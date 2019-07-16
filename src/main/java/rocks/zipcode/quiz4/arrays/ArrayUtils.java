95d7427729c65de257a5f69af11aa429b4a6839f
AngelicaSantos <angelica.santos2113@gmail.com>
2019-07-16 10:44:38 -0400

    ArraysComplete

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,45 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+
+        String middle = values[(values.length /2)];
+
+
+        return middle;
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+      String[] blahArray = new String[values.length-1];
+      for (int i=0; i<blahArray.length/2;i++) {
+          blahArray[i] = values [i];
+      }
+      for (int i= values.length/2; i<blahArray.length;i++) {
+          blahArray[i] = values[i+1];
+      }
+      return blahArray;
     }
 
+
     public static String getLastElement(String[] values) {
-        return null;
+        String last = values[values.length-1];
+
+        return last;
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+
+        String[] wowArray = new String[values.length-1];
+        for(int i =0; i < wowArray.length; i++){
+            wowArray[i] = values[i];
+        }
+        return wowArray;
     }
 }
